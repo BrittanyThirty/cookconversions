95000d89b44c22ad3a294af8426b8859881c9e0b
BrittanyThirty <bmkthirty@gmail.com>
2018-01-11 01:06:53 -0800

    content added per instructions on the learnhowtocode.com website branching pratice and button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-$(document).touch() {
+$(document).ready (function() {
 
-});
+    var age = parseInt(prompt("How old are you?")); //be sure to recall the prompt always returns string so use parseInt//
+
+    if (age > 21) {
+      $('#drinks').show();
+    } else if (age === 21) {
+      alert("Now don't go crazy!");
+      $('#drinks').show();
+    } else {
+      $('#under-21').show();
+    }
+  });
